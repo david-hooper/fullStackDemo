2b1c8e91fdd88c662f12d546fa4bcfecef6744cc
david-hooper <david.hooper@galvanize.com>
2023-04-04 17:19:49 -0400

    troubleshooting

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+import dotenv from 'dotenv';
+import postgres from "postgres";
+import express from 'express'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+dotenv.config();
+//let db_URL = ;
+const sql = postgres(process.env.DATABASE_URL);
+app.use(express.json());
+app.use(express.static("public"))
+app.get('/', (req, res) => {
+    res.json({"hello":"world"})
+})
+app.get('/students', (req, res) => {
+    sql`SELECT * FROM pg_database;`.then((result)=> {
+        //res.json(result);
+    })
+})
+
+app.listen(port, (error) => {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        console.log(`Listening on port: ${port}`)
+    }
+})
